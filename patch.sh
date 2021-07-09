805b10d68f328c5c98fcf35f16eccd775e1c12c6
Kara Alexandra <ardnaxelarak@gmail.com>
2021-07-09 09:45:21 -0700

    Properly set output filename

--- patch.sh
+++ patch.sh
@@ -5,21 +5,23 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-output="output/$(basename $1 .asm).sfc"
-cp ../working.sfc $output
-patches=("no_flash.asm" "quickswap.asm")
-defines=()
+PATCHES=("no_flash.asm" "quickswap.asm")
+DEFINES=()
 for var in "$@"; do
 	if [[ $var = --* ]]; then
 		var=${var#"--"}
-		defines+=(-D$var)
+		DEFINES+=(-D$var)
 	elif [[ -f "$var" ]]; then
-		patches+=($var)
+		OUTPUT="${OUTPUT:-output/$(basename $var .asm).sfc}"
+		PATCHES+=($var)
 	else
 		echo file \"$var\" not found
 		exit 1
 	fi
 done
-for var in ${patches[@]}; do
-	../z3randomizer/asar $defines $var $output
+OUTPUT="${OUTPUT:-output/quickswap.sfc}"
+
+cp ../working.sfc $OUTPUT
+for var in ${PATCHES[@]}; do
+	../z3randomizer/asar $DEFINES $var $OUTPUT
 done

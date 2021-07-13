b0bcba1005249071dbd2074b10cb49168dcb8a83
Kara Alexandra <ardnaxelarak@gmail.com>
2021-07-13 11:40:37 -0700

    Flip ganon_item condition to simplify

--- ganon_variable.asm
+++ ganon_variable.asm
@@ -1,7 +1,7 @@
 incsrc fast_ganon.asm
 org $30802E
-if not(defined("ganon_item"))
-	db #$0C ; ganon vulnerable to hammer
-else
+if defined("ganon_item")
 	db #$!ganon_item
+else
+	db #$0C ; ganon vulnerable to hammer
 endif
